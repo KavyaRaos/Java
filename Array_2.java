6aa278f58b6568ddcf33a12800b15a11ce9efb68
KavyaRaos <kavyaraos9896@gmail.com>
2024-03-31 10:32:35 +0530

    info of reversing odd array length elements

--- Array_2.java
+++ Array_2.java
@@ -2,7 +2,8 @@ import java.util.*;
 public class Array_2 {
     public static void main(String[] args) {
         Scanner sc =new Scanner(System.in);
-        int [] arr = new int[8];
+        int [] arr = new int[5];
+        //int [] arr = new int [8];
         int j = arr.length-1;
         int temp = 0;
         System.out.println("Enter the numbers:");
@@ -15,6 +16,8 @@ public class Array_2 {
             arr[j] = temp;
             j--;
         }
+
+        //Even though if you have odd number of array length the code will not change , only remember to reverse from index 0 to mid index.
 
         System.out.println("______________________");
         for (int n : arr){
